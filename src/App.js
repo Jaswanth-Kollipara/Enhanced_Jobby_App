baaa5490e13bc3bfe40ccc8972f4e14d9d52f948
Jaswanth-Kollipara <kolliparajaswanth5@gmail.com>
2024-06-10 05:29:19 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => (
     <Switch>
       <Route exact path="/login" component={Login} />
       <ProtectedRoute exact path="/" component={Home} />
-      <ProtectedRoute exact path="/jobs" component={Jobs} />
       <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
       <Route path="/not-found" component={NotFound} />
       <Redirect to="not-found" />
     </Switch>
